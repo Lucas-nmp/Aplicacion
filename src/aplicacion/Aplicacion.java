08ca40076a3df98bbe744e275745a0f532828e9f
lucas-nmp <lucas.morandeira.parejo@gmail.com>
2022-08-25 22:03:29 +0100

    Creación del proyecto

---
+++ src/aplicacion/Aplicacion.java
@@ -0,0 +1,12 @@
+package aplicacion;
+/**
+ * @author lucas
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("hola");
+        
+    }
+    
+}
